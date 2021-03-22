0439cc5d45037de4dab88a807e18f8446ac64a7f
pilaralv <70604952+pilaralv@users.noreply.github.com>
2021-03-22 11:34:50 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,30 +1,3 @@
-# Setting working directory
-setwd("/Users/alvarezjerezp2/Desktop/coursera_r/")
-
-# Downloading necessary packages
-library(dplyr)
-
-# Reading in data
-filename <- "project_2.zip"
-if(!file.exists(filename)){
-        file_URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-        download.file(file_URL, filename, method="curl")
-}
-
-# Creating and unzipping folder
-if(!file.exists("UCI Har Dataset")){
-        unzip(filename)
-}
-
-# Next we need to create the variables to go in our dataframe
-features <- read.table("./UCI HAR Dataset/features.txt",col.names=c("number", "funcs"))
-activities <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("code","activity"))
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = features$funcs)
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "code")
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features$funcs)
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
 # Starting run_analysis.R
 # We need to merge our dataframes together based on the ones above
